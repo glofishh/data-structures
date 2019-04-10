1e5a5334b905cec2fd74e9de1617a5ee0a264a0e
Camron Stuart <Samlennder@gmail.com>
2019-04-10 18:45:49 -0400

    Finish queue, pretty jancky

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,14 +6,47 @@ var Queue = function() {
 
   // Implement the methods below
 
+  let size = 0
   someInstance.enqueue = function(value) {
+    //enqueue from the back
+    storage[size] = value
+    size ++
+    Object.assign(someInstance, storage)
   };
 
   someInstance.dequeue = function() {
+    //dequeue from front
+    if(size === 0){
+      return undefined
+    }
+    size--
+    let dequeued = storage[0]
+
+    for(let key in storage){
+      storage[Number(key) - 1] = storage[key]
+      someInstance[Number(key) - 1] = someInstance[key]
+    }
+
+
+    return dequeued
+
   };
 
   someInstance.size = function() {
+    return size
   };
 
   return someInstance;
 };
+
+
+debugger
+let queue = Queue()
+
+queue.enqueue('a');
+queue.enqueue('b');
+queue.dequeue();
+queue.enqueue('c');
+
+console.log(queue.dequeue()) //b
+console.log
