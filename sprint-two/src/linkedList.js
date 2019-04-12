aa8cfb5b24853fd2575a784dfbc34d8f5b03c889
Camron Stuart <Samlennder@gmail.com>
2019-04-12 18:09:25 -0400

    Complete Linked and Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,14 +3,63 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // takes value and adds to end of list
+  // add a new Node
+  // link that Node to tail
+  // if empty link that node to head
+  // if the list was not empty change the 'next' property of the last added node to point to the newly added node
+  //
+
   list.addToTail = function(value) {
+    var newKey = Math.random().toFixed(4).toString().replace(/^0./, '')
+                     //randomly-generated number, cut to 4, convert to string to use as key later, .replace to remove decimal to get a string of 4 completely random numbers
+                     //what the key will be everytime
+    //if first thing being added
+    if(list.tail === null) {
+      list.tail = new Node(value)
+      list.head = list.tail
+    } else if (list.head.next === null) {
+      //then only one thing in list
+      list.tail = new Node(value)
+      list.head.next = 'tail'
+    } else if(Object.keys(list).length === 5) {
+      //we have head, tail, adding next thing
+      list[newKey] = list.tail //bumps tail back to newKey
+      list[newKey].next = 'tail'
+      list.tail = new Node(value) //regular case once head and tail are established; pushing to one side
+      list.head.next = newKey //makes head -> new node we added to list
+    } else{
+      //when adding a 4+ item to list (after head, tail, methods)
+      //same as above, except next value of head node is not changed
+      list[newKey] = list.tail
+      list[newKey].next = 'tail'
+      list.tail = new Node(value)
+    }
+
+
   };
 
+  //removes first node from list and returns its value
   list.removeHead = function() {
+    let removed = list.head.value //saves value of node at head
+    list.head = list[list.head.next] // goes into head, look at next, and make new head next
+    return removed
   };
 
+  //returns boolean reflecting whether or not the passed-in value is in the linked list
   list.contains = function(target) {
+    //starts i at head, then do in loop as long i isn't null, keeps setting i to wahtever it was looking at's next value (whatever holding in next value); when it hits tail, next val not set yet, so it stops
+      //if list.head value is the target, return true
+      //if you managed to make through loop without triggering true, return false
+    for(i = 'head'; i !== null; i = list[i].next){
+      if(list[i].value === target){
+        return true
+      }
+    }
+    return false
   };
+
+
 
   return list;
 };
@@ -27,3 +76,18 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+debugger
+
+let list = new LinkedList
+list.addToTail('1')
+list.addToTail('2')
+list.addToTail('3')
+list.contains('2')
+console.log(list)
+console.log(list.head)
+
+list.addToTail(4);
+list.addToTail(5);
+console.log(list.contains(4))
+console.log(list.contains(5))
+console.log(list.contains(6))
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
+//
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  Object.assign(newTree, treeMethods)
 
   return newTree;
 };
@@ -11,9 +13,30 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  this.children.push(Tree(value))
 };
 
 treeMethods.contains = function(target) {
+  //if contains, return true
+  //should correctly detect nested children
+    //--> if has children, recursive call on each child
+    // else, return false
+    let result = false
+
+    function search (tree){
+    if(tree.value === target){
+      result = true
+    } else if(tree.children.length > 0){
+      for(let i = 0; i < tree.children.length; i++){
+        search(tree.children[i])
+      }
+    }
+  }
+  search(this)
+
+  return result
+
 };
 
 
@@ -21,3 +44,8 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var tree = Tree()
+
+debugger
+tree.addChild(5);
+console.log(tree.contains(5));
