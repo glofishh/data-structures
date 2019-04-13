4921d6ce005624b8f76e716a47df50b63d4435a0
Camron Stuart <Samlennder@gmail.com>
2019-04-13 14:38:14 -0400

    Add passing code for Set and HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,54 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //          {[ [ [k, v] ], []]}
+  //[key, value], [key, value]
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if(!this._storage.get(index)){
+    this._storage.set(index, [[key, value]])
+  } else {
+    let thisBucket = this._storage.get(index)
+    for(let i = 0; i < thisBucket.length; i++){
+      if(thisBucket[i][0] === key){
+        thisBucket[i][1] = value
+        return undefined
+      }
+    }
+    thisBucket.push([key, value])
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index)){
+    let thisBucket = this._storage.get(index)
+    if(thisBucket.length > 1){
+      for(let i = 0; i < thisBucket.length; i++){
+        if(thisBucket[i][0] === k){
+          return thisBucket[i][1]
+        }
+      }
+    }
+    if(this._storage.get(index).length > 0){
+      return this._storage.get(index)[0][1]
+    }
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let thisBucket = this._storage.get(index)
+  for(let i = 0; i < thisBucket.length; i++){
+    console.log(thisBucket[i])
+    if(thisBucket[i][0] === k){
+      return thisBucket.splice(i, 1)
+    }
+  }
+  // [     [[k1, undefined], [k2,v], [k3, undefined]]      ]
 };
 
 
@@ -22,5 +58,53 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var LimitedArray = function(limit) {
+  var storage = [];
+
+  var limitedArray = {};
+  limitedArray.get = function(index) {
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value) {
+    checkLimit(index);
+    storage[index] = value;
+  };
+  limitedArray.each = function(callback) {
+    for (var i = 0; i < storage.length; i++) {
+      callback(storage[i], i, storage);
+    }
+  };
+
+  var checkLimit = function(index) {
+    if (typeof index !== 'number') {
+      throw new Error('setter requires a numeric index for its first argument');
+    }
+    if (limit <= index) {
+      throw new Error('Error trying to access an over-the-limit index');
+    }
+  };
+
+  return limitedArray;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+var getIndexBelowMaxForKey = function(str, max) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
 
 
+let hashTable = new HashTable()
+
+ debugger
+ hashTable.insert('Steven', 'Tyler');
+ hashTable.remove('Steven');
+console.log(hashTable.retrieve('Steven'))
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,51 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(!this.contains(item)){
+    this._storage[this.size()] = item
+  }
 };
 
 setPrototype.contains = function(item) {
+  for(let key in this._storage){
+    if(this._storage[key] === item){
+      return true
+    }
+  }
+  return false
 };
 
-setPrototype.remove = function(item) {
+//adding index to remove = can remove by index
+setPrototype.remove = function(item, index) {
+  if(index){
+    delete this._storage[index]
+  }
+  for(let key in this._storage){
+    if(this._storage[key] === item){
+      delete this._storage[key]
+    }
+  }
 };
+
+setPrototype.size = function(){
+  return Object.keys(this._storage).length
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ let set = Set()
+
+ debugger
+set.add('Susan Sarandon');
+set.add('Danny Glover');
+set.contains('Danny Glover')
+set.contains('Susan Sarandon')
