ddabcf467f2e437eff47508a20703707ebeb6382
Camron Stuart <Samlennder@gmail.com>
2019-04-13 19:02:04 -0400

    Complete binarySearchTree and advanced HashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
+  this.value = value
+  this.left
+  this.right
+
 };
+
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+BinarySearchTree.prototype.insert = function(value){
+  if(value > this.value){
+    if(this.right){
+      this.right.insert(value)
+    } else {
+      this.right = new BinarySearchTree(value)
+    }
+  } else {
+    if(this.left){
+      this.left.insert(value)
+    } else{
+      this.left = new BinarySearchTree(value)
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  if(this.value === value){
+    return true
+  }
+  if((value > this.value) && this.right){
+    return this.right.contains(value)
+  }
+  if((value < this.value) && this.left){
+    return this.left.contains(value)
+  }
+
+  return false
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(iterator){
+  iterator(this.value)
+
+  if(this.left){
+    this.left.depthFirstLog(iterator)
+  }
+  if(this.right){
+    this.right.depthFirstLog(iterator)
+  }
+};
+
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,6 +2,7 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this.count = 0
   this._storage = LimitedArray(this._limit);
   //          {[ [ [k, v] ], []]}
   //[key, value], [key, value]
@@ -11,6 +12,7 @@ HashTable.prototype.insert = function(key, value) {
   var index = getIndexBelowMaxForKey(key, this._limit);
   if(!this._storage.get(index)){
     this._storage.set(index, [[key, value]])
+    this.count++
   } else {
     let thisBucket = this._storage.get(index)
     for(let i = 0; i < thisBucket.length; i++){
@@ -19,7 +21,12 @@ HashTable.prototype.insert = function(key, value) {
         return undefined
       }
     }
+
     thisBucket.push([key, value])
+    this.count++
+    if(this.count > this._limit * .75){
+      this._resize(this._limit * 2)
+    }
   }
 };
 
@@ -45,66 +52,37 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   let thisBucket = this._storage.get(index)
   for(let i = 0; i < thisBucket.length; i++){
-    console.log(thisBucket[i])
     if(thisBucket[i][0] === k){
-      return thisBucket.splice(i, 1)
+      this.count--
+      let val = thisBucket.splice(i, 1)[1]
+      if(this.count < this._limit * .25){
+        this._resize(this._limit / 2)
+      }
+      return val
     }
   }
   // [     [[k1, undefined], [k2,v], [k3, undefined]]      ]
 };
+HashTable.prototype._resize = function(newLimit){
+  var oldStorage = this._storage
+  this._limit = newLimit
+  this._storage = LimitedArray(newLimit)
+  this.count = 0
+  let context = this;
+
+  oldStorage.each(function(bucket){
+    if(!bucket){
+      return undefined
+    }
+    for (var i = 0; i < bucket.length; i++){
+      var tuple = bucket[i]
+      context.insert(tuple[0], tuple[1])
+    }
+  })
+  };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-var LimitedArray = function(limit) {
-  var storage = [];
-
-  var limitedArray = {};
-  limitedArray.get = function(index) {
-    checkLimit(index);
-    return storage[index];
-  };
-  limitedArray.set = function(index, value) {
-    checkLimit(index);
-    storage[index] = value;
-  };
-  limitedArray.each = function(callback) {
-    for (var i = 0; i < storage.length; i++) {
-      callback(storage[i], i, storage);
-    }
-  };
-
-  var checkLimit = function(index) {
-    if (typeof index !== 'number') {
-      throw new Error('setter requires a numeric index for its first argument');
-    }
-    if (limit <= index) {
-      throw new Error('Error trying to access an over-the-limit index');
-    }
-  };
-
-  return limitedArray;
-};
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between the
-// numbers 0 and `max`
-var getIndexBelowMaxForKey = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
-  }
-  return hash % max;
-};
-
-
-let hashTable = new HashTable()
-
- debugger
- hashTable.insert('Steven', 'Tyler');
- hashTable.remove('Steven');
-console.log(hashTable.retrieve('Steven'))
